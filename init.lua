5a15cfab456f810aeeaea6b47a5a8f86f1c74643
thiagocantarelli <thiagocantarelli123@gmail.com>
2026-05-19 20:04:09 +0100

    19.05.26 Porting to macOS

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
------ Options ------
-vim.o.guicursor= ""
+---- Options ------
+vim.o.guicursor = ""
 vim.g.mapleader = " "
 vim.o.number = true
 vim.o.rnu = true
@@ -11,10 +11,10 @@ vim.o.incsearch = true
 
 vim.o.sidescrolloff = 32
 
-
-vim.o.softtabstop = 0
-vim.o.shiftwidth = 0
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.o.smartindent = true
+
 
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 32
@@ -24,84 +24,97 @@ vim.opt.isfname:append("@-@")
 ----- Keymaps ------
 
 -- MOVE LINES UP AND DOWN
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Moves Line Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moves Line Up" })
 
 -- CTRL-C = ESC
-vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Esc" })
 
 -- COPY TO CLIPBOARD
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to Clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy to clipboard" })
 
 -- OPEN TERMINAL WINDOW
-vim.keymap.set("n", "<leader>tr", ":term<CR>i")
+vim.keymap.set("n", "<leader>tr", ":term<CR>i", { desc = "Open terminal Window" })
 vim.keymap.set("n", "<leader>st", function()
-    vim.cmd.vnew()
-    vim.cmd.term()
-    vim.cmd.wincmd("L")
-    vim.api.nvim_win_set_width(0, 100)
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("L")
+	vim.api.nvim_win_set_width(0, 100)
 end)
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
 
-vim.api.nvim_set_keymap('t', '<C-l><C-l>', [[<C-\><C-N>:lua ClearTerm(0)<CR>]], {})
-vim.api.nvim_set_keymap('t', '<C-l><C-l><C-l>', [[<C-\><C-N>:lua ClearTerm(1)<CR>]], {})
+vim.api.nvim_set_keymap("t", "<C-l><C-l>", [[<C-\><C-N>:lua ClearTerm(0)<CR>]], {})
+vim.api.nvim_set_keymap("t", "<C-l><C-l><C-l>", [[<C-\><C-N>:lua ClearTerm(1)<CR>]], {})
 
 function ClearTerm(reset)
-  vim.opt_local.scrollback = 1
+	vim.opt_local.scrollback = 1
 
-  vim.api.nvim_command("startinsert")
-  if reset == 1 then
-    vim.api.nvim_feedkeys("reset", 't', false)
-  else
-    vim.api.nvim_feedkeys("clear", 't', false)
-  end
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<cr>', true, false, true), 't', true)
+	vim.api.nvim_command("startinsert")
+	if reset == 1 then
+		vim.api.nvim_feedkeys("reset", "t", false)
+	else
+		vim.api.nvim_feedkeys("clear", "t", false)
+	end
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<cr>", true, false, true), "t", true)
 
-  vim.opt_local.scrollback = 10000
+	vim.opt_local.scrollback = 10000
 end
 
 ----- Plugins ------
 local function gh(x)
-        return "https://github.com/" .. x
+	return "https://github.com/" .. x
 end
-vim.cmd.colorscheme("retrobox")
 
-vim.pack.add({gh("/nvim-lualine/lualine.nvim")})
-require('lualine').setup({
-        options = {
-                theme = 'codedark'
-        }
+-- Markdown Preview --
+vim.pack.add({ gh('MeanderingProgrammer/render-markdown.nvim') })
+
+----- Colorscheme ------
+vim.pack.add({ gh("EdenEast/nightfox.nvim") })
+vim.cmd.colorscheme("nordfox")
+-- vim.pack.add({gh("craftzdog/solarized-osaka.nvim")})
+-- vim.cmd.colorscheme("solarized-osaka")
+
+-- vim.cmd.colorscheme("retrobox")
+
+vim.pack.add({ gh("/nvim-lualine/lualine.nvim") })
+require("lualine").setup({
+	options = {
+		theme = "nightfox",
+	},
 })
-vim.pack.add({gh("nvim-lua/plenary.nvim")})
-
-
-
-
-
+vim.pack.add({ gh("nvim-lua/plenary.nvim") })
 
 -- TREESITTER --
 vim.pack.add({ gh("nvim-treesitter/nvim-treesitter") })
 local treesitter = require("nvim-treesitter")
 treesitter.setup({
-    install_dir = vim.fn.stdpath('data') .. '/site',
-    highlight = { enable = true },
-    indent = { enable = true },
+	install_dir = vim.fn.stdpath("data") .. "/site",
+	highlight = { enable = true },
+	indent = { enable = true },
 })
 treesitter.install({
-    "lua", "python", "go", "html", "json", "xml", "sql", "templ", "javascript",
+	"lua",
+	"python",
+	"go",
+	"html",
+	"json",
+	"xml",
+	"sql",
+	"templ",
+	"javascript",
 })
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-        local lang = vim.treesitter.language.get_lang(args.match)
-        if vim.list_contains(treesitter.get_available(), lang) then
-            if not vim.list_contains(treesitter.get_installed(), lang) then
-                treesitter.install(lang):wait()
-            end
-            vim.treesitter.start(args.buf)
-        end
-    end,
-    desc = "Enable nvim-treesitter and install parser if not installed"
+	callback = function(args)
+		local lang = vim.treesitter.language.get_lang(args.match)
+		if vim.list_contains(treesitter.get_available(), lang) then
+			if not vim.list_contains(treesitter.get_installed(), lang) then
+				treesitter.install(lang):wait()
+			end
+			vim.treesitter.start(args.buf)
+		end
+	end,
+	desc = "Enable nvim-treesitter and install parser if not installed",
 })
 ---------
 vim.pack.add({ gh("brenoprata10/nvim-highlight-colors") })
@@ -165,34 +178,28 @@ require("nvim-highlight-colors").setup({
 	-- exclude_buffer = function(bufnr) end,
 })
 
-
-
-
-
-
-
-
 -- LSP --
 vim.pack.add({ gh("nvimtools/none-ls.nvim") })
 local null_ls = require("null-ls")
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.pyink,
-    },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.pyink,
+	},
 })
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Text" })
 
+-- Completition --
 vim.pack.add({
-    gh("hrsh7th/cmp-nvim-lsp"),
-    gh("saadparwaiz1/cmp_luasnip"),
-    gh("rafamadriz/friendly-snippets"),
-    gh("L3MON4D3/LuaSnip"),
-    gh("hrsh7th/nvim-cmp"),
+	gh("hrsh7th/cmp-nvim-lsp"),
+	gh("saadparwaiz1/cmp_luasnip"),
+	gh("rafamadriz/friendly-snippets"),
+	gh("L3MON4D3/LuaSnip"),
+	gh("hrsh7th/nvim-cmp"),
 })
 local cmp = require("cmp")
 local luasnip = require("luasnip")
@@ -256,94 +263,91 @@ cmp.setup({
 })
 
 vim.pack.add({
-    gh("williamboman/mason.nvim"),
+	gh("williamboman/mason.nvim"),
 	gh("williamboman/mason-lspconfig.nvim"),
 	gh("neovim/nvim-lspconfig"),
 })
 require("mason").setup()
 require("mason-lspconfig").setup({
-   ensure_installed = {
-   	"lua_ls",
-   	-- "pyright",
-    -- "pylsp",
-   	"html",
-        -- "gopls",
-   	"templ",
-   	"jsonls",
-   	"cssls",
-   	"tailwindcss",
-   	"ts_ls",
-   	"clangd",
-   	--"htmx",
-   	"denols",
-   	-- "rust_analyzer",
-   },
+	ensure_installed = {
+		"lua_ls",
+		-- "pyright",
+		-- "pylsp",
+		"html",
+		-- "gopls",
+		"templ",
+		"jsonls",
+		"cssls",
+		"tailwindcss",
+		"ts_ls",
+		"clangd",
+		--"htmx",
+		"denols",
+		-- "rust_analyzer",
+	},
 })
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-vim.lsp.config('*', {
-  capabilities = capabilities,
+vim.lsp.config("*", {
+	capabilities = capabilities,
 })
 
 vim.lsp.config.lua_ls = {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" }
-            }
-        }
-    }
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 }
 
 vim.lsp.config.clangd = {
-  cmd = {
-    'clangd',
-    '--clang-tidy',
-    '--background-index',
-    '--offset-encoding=utf-8',
-  },
-  root_markers = { '.clangd', 'compile_commands.json' },
-  filetypes = { 'c', 'cpp' },
+	cmd = {
+		"clangd",
+		"--clang-tidy",
+		"--background-index",
+		"--offset-encoding=utf-8",
+	},
+	root_markers = { ".clangd", "compile_commands.json" },
+	filetypes = { "c", "cpp" },
 }
 vim.lsp.config.gdscript = {
-    name = "godot",
-    cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+	name = "godot",
+	cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
 }
 vim.lsp.config.tailwindcss = {
-    include_languages = { html =  "templ" },
+	include_languages = { html = "templ" },
 	capabilities = capabilities,
 }
 vim.diagnostic.config({ underline = false })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {desc = "Code Actions"})
 
 vim.pack.add({ gh("mfussenegger/nvim-dap") })
 local dap = require("dap")
 dap.adapters.godot = {
-    type = "server",
-    host = "127.0.0.1",
-    port = 6006,
+	type = "server",
+	host = "127.0.0.1",
+	port = 6006,
 }
 dap.configurations.gdscript = {
-    {
-        type = "godot",
-        request = "launch",
-        name = "Launch scene",
-        project = "${workspaceFolder}",
-        launch_scene = true,
-    },
+	{
+		type = "godot",
+		request = "launch",
+		name = "Launch scene",
+		project = "${workspaceFolder}",
+		launch_scene = true,
+	},
 }
 
-
-
-
 -- TELESCOPE --
-local install_telescope_fzf = function (ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
-        vim.system({ "make" }, { cwd = ev.data.path }):wait()
-    end
+local install_telescope_fzf = function(ev)
+	local name, kind = ev.data.spec.name, ev.data.kind
+	if name == "telescope-fzf-native.nvim" and (kind == "install" or kind == "update") then
+		vim.system({ "make" }, { cwd = ev.data.path }):wait()
+	end
 end
 vim.api.nvim_create_autocmd("PackChanged", { callback = install_telescope_fzf })
 vim.pack.add({ gh("nvim-lua/plenary.nvim") })
@@ -352,42 +356,73 @@ vim.pack.add({ gh("nvim-telescope/telescope.nvim") })
 vim.pack.add({ gh("nvim-telescope/telescope-ui-select.nvim") })
 local telescope = require("telescope")
 telescope.setup({
-    extensions = {
-        fzf = {},
-        ["ui-select"] =  {
-            require("telescope.themes").get_dropdown({}),
-        }
-    },
-    defaults = {
-        file_ignore_patterns = { "node_modules", "venv", "__pycache__" },
-    },
+	extensions = {
+		fzf = {},
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+	defaults = {
+		file_ignore_patterns = { "node_modules", "venv", "__pycache__" },
+	},
 })
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find FIles" })
 vim.keymap.set("n", "<C-b>", builtin.buffers, {})
-vim.keymap.set("n", "<C-h>", builtin.command_history, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>mp", builtin.man_pages, {})
+vim.keymap.set("n", "<C-h>", builtin.command_history, {desc =  "Command History"})
+vim.keymap.set("n", "<leader>mp", builtin.man_pages,  {desc = "Man Page"})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep,  {desc = "Live Grep"})
 ---------
 
 -- NEO TREE --
 vim.pack.add({
-  {
-    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
-    version = vim.version.range('3')
-  },
-  -- dependencies
-  "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/MunifTanjim/nui.nvim",
-  -- optional, but recommended
-  "https://github.com/nvim-tree/nvim-web-devicons",
+	{
+		src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+		version = vim.version.range("3"),
+	},
+	-- dependencies
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	-- optional, but recommended
+	"https://github.com/nvim-tree/nvim-web-devicons",
 })
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-
-
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {desc = "Neotree Toggle"})
 
 vim.pack.add({ gh("mrcjkb/rustaceanvim") })
+
+-- Which Key --
+vim.pack.add({ gh("folke/which-key.nvim") })
+local wk = require('which-key')
+wk.add({
+        {"<leader>a", group = "AI"}, 
+        {"<leader>c", group = "Code"},
+        {"<leader>f", group = "Files"},
+        {"<leader>m", group = "Manual"},
+        {"<leader>g", group = "Format"},
+        {"<leader>s", group = "Split Terminal"},
+        {"<leader>t", group = "Terminal"}
+
+})
+
+
+vim.pack.add({
+	gh("coder/claudecode.nvim"),
+})
+local claudecode = require("claudecode")
+claudecode.setup()
+
+-- AI/Claude Code keybindings
+vim.keymap.set("n", "<leader>ac", ":ClaudeCode<CR>", { desc = "Toggle Claude" })
+vim.keymap.set("n", "<leader>af", ":ClaudeCodeFocus<CR>", { desc = "Focus Claude" })
+vim.keymap.set("n", "<leader>ar", ":ClaudeCode --resume<CR>", { desc = "Resume Claude" })
+vim.keymap.set("n", "<leader>aC", ":ClaudeCode --continue<CR>", { desc = "Continue Claude" })
+vim.keymap.set("n", "<leader>am", ":ClaudeCodeSelectModel<CR>", { desc = "Select Claude model" })
+vim.keymap.set("n", "<leader>ab", ":ClaudeCodeAdd %<CR>", { desc = "Add current buffer" })
+vim.keymap.set("v", "<leader>as", ":ClaudeCodeSend<CR>", { desc = "Send to Claude" })
+vim.keymap.set("n", "<leader>aa", ":ClaudeCodeDiffAccept<CR>", { desc = "Accept diff" })
+vim.keymap.set("n", "<leader>ad", ":ClaudeCodeDiffDeny<CR>", { desc = "Deny diff" })
+
 --------
